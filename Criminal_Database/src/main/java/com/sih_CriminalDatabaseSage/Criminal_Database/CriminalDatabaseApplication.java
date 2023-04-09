57b6d75d0a6fedd62107e32d74ffd292f504720b
Archi kirar <archikirar@gmail.com>
2023-04-09 15:39:14 +0530

    Backend setup
    Spring Boot Maven app

---
+++ Criminal_Database/src/main/java/com/sih_CriminalDatabaseSage/Criminal_Database/CriminalDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.sih_CriminalDatabaseSage.Criminal_Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriminalDatabaseApplication.class, args);
+	}
+
+}
---
+++ Criminal_Database/src/test/java/com/sih_CriminalDatabaseSage/Criminal_Database/CriminalDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih_CriminalDatabaseSage.Criminal_Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
